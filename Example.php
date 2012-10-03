0a686a79ad3b618ed09406b0c943efabd6ee8056
Claus Due <claus@wildside.dk>
2012-10-03 23:31:57 +0200

    [TASK] Wrapper class to allow simple instantiation
    It is now possible to $view = new \TYPO3\Fluid\FluidTemplate(); and use
    constructor arguments to configure template file, paths and variables.
    
    Example script also updated with detailed comments about what is being
    done and why.

--- Example.php
+++ Example.php
@@ -1,25 +1,75 @@
 <?php
 
-define('CLASS_BASE_DIR', __DIR__);
+/**
+ * Initial constants
+ *
+ * These are only necessary for this example; however, CLASS_CACHE_DIR
+ * is temporarily required (until a ConfigurationManager can be
+ * backported in a sensible incarnation).
+ */
+$baseDir = __DIR__;
+	// Constant used by the ClassLoader below. Only necessary for this particular ClassLoader to work.
+define('CLASS_BASE_DIR', $baseDir);
+	// Cache dir where TYPO3 Fluid will write pre-compiled PHP class files
 define('CLASS_CACHE_DIR', __DIR__ . '/Cache/');
 
-$baseDir = __DIR__;
+/**
+ * Class loading
+ *
+ * The next five lines initialize a ClassLoader that is built into the
+ * ObjectManager class. If class loading is not to be enabled (for example
+ * when this is already provided by a framework such as Symfony) simply
+ * skip these require_once lines and class loader initializations.
+ */
 require_once $baseDir . '/TYPO3/Fluid/Object/ObjectManagerInterface.php';
 require_once $baseDir . '/TYPO3/Fluid/Object/ObjectManager.php';
-$loader = new \TYPO3\Fluid\Object\ObjectManager('TYPO3', __DIR__);
+$loader = new \TYPO3\Fluid\Object\ObjectManager('TYPO3', $baseDir);
 $loader->setIncludePath(__DIR__);
 $loader->register();
 
+/**
+ * Template resource configuration
+ *
+ * These next variables are what is necessary in order to render a Fluid
+ * template that uses all the available constructs; Layouts, Partials and variables.
+ * Additional information can be found in the example template itself.
+ */
+$templatePathAndFilename = $baseDir . '/Example/Templates/Example.html';
+$layoutRootPath = $baseDir . '/Example/Layouts/';
+$partialRootPath = $baseDir . '/Example/Partials/';
 $variables = array(
 	'foo' => 'not bar',
 	'bar' => 'not foo',
 );
-/** @var \TYPO3\Fluid\View\StandaloneView $view  */
-$view = $loader->create('TYPO3\\Fluid\\View\\StandaloneView');
-$view->setLayoutRootPath($baseDir . '/Example/Layouts/');
-$view->setPartialRootPath($baseDir . '/Example/Partials/');
-$view->setTemplatePathAndFilename($baseDir . '/Example/Templates/Example.html');
-$view->setTemplateCacheDir(CLASS_CACHE_DIR);
+
+/**
+ * Initialization
+ *
+ * All arguments except $templatePathAndFilename are optional.
+ * $variables must be an array.
+ *
+ * If constructor arguments are not desired there is an alternative below.
+ *
+ * We then allow TYPO3 Fluid to create pre-compiled PHP class versions of our
+ * template files by setting the template cache dir.
+ */
+$view = new \TYPO3\Fluid\FluidTemplate($templatePathAndFilename, $variables, $layoutRootPath, $partialRootPath);
+
+/**
+ * Alternative configuration method
+ *
+ * Below is (redundant but harmless) an alternative method of specifying paths, template,
+ * variables etc.
+ *
+ * The order is not important but in this example we follow the above argument sequence.
+ */
+$view->setTemplatePathAndFilename($templatePathAndFilename);
+$view->assignMultiple($variables);
+$view->setLayoutRootPath($layoutRootPath);
+$view->setPartialRootPath($partialRootPath);
+	// assign one last variable used by the Example template for illustrations
 $view->assign('assignedVariables', $variables);
 
+	// no comment necessary
 echo $view->render();
+exit();
--- TYPO3/Fluid/FluidTemplate.php
+++ TYPO3/Fluid/FluidTemplate.php
@@ -1,23 +1,147 @@
 <?php
 namespace TYPO3\Fluid;
 
-	/*                                                                        *
-	 * This script is part of the TYPO3 project - inspiring people to share!  *
-	 *                                                                        *
-	 * TYPO3 is free software; you can redistribute it and/or modify it under *
-	 * the terms of the GNU General Public License version 2 as published by  *
-	 * the Free Software Foundation.                                          *
-	 *                                                                        *
-	 * This script is distributed in the hope that it will be useful, but     *
-	 * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
-	 * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
-	 * Public License for more details.                                       *
-	 *                                                                        */
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
 /**
 
  */
-class FluidTemplate {
+class FluidTemplate implements \TYPO3\Fluid\View\ViewInterface {
 
+	/**
+	 * @var \TYPO3\Fluid\View\StandaloneView
+	 */
+	protected $view;
 
+	/**
+	 * @var \TYPO3\Fluid\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @param string $templateFile
+	 * @param array $variables
+	 * @param string $layoutRootPath
+	 * @param string $partialRootPath
+	 */
+	public function __construct($templateFile=NULL, array $variables=array(), $layoutRootPath=NULL, $partialRootPath=NULL) {
+		$this->objectManager = new \TYPO3\Fluid\Object\ObjectManager();
+		$this->view = $this->objectManager->create('\\TYPO3\\Fluid\\View\\StandaloneView');
+		if ($templateFile !== NULL) {
+			$this->view->setTemplatePathAndFilename($templateFile);
+		}
+		if (count($variables) > 0) {
+			$this->view->assignMultiple($variables);
+		}
+		if ($layoutRootPath) {
+			$this->view->setLayoutRootPath($layoutRootPath);
+		}
+		if ($partialRootPath) {
+			$this->view->setPartialRootPath($partialRootPath);
+		}
+	}
+
+	/**
+	 * Add a variable to the view data collection.
+	 * Can be chained, so $this->view->assign(..., ...)->assign(..., ...); is possible
+	 *
+	 * @param string $key Key of variable
+	 * @param mixed $value Value of object
+	 * @return \TYPO3\Fluid\View\ViewInterface an instance of $this, to enable chaining
+	 * @api
+	 */
+	public function assign($key, $value) {
+		$this->view->assign($key, $value);
+	}
+
+	/**
+	 * Add multiple variables to the view data collection
+	 *
+	 * @param array $values array in the format array(key1 => value1, key2 => value2)
+	 * @return \TYPO3\Fluid\View\ViewInterface an instance of $this, to enable chaining
+	 * @api
+	 */
+	public function assignMultiple(array $values) {
+		$this->view->assignMultiple($values);
+	}
+
+	/**
+	 * Renders the view
+	 *
+	 * @return string The rendered view
+	 * @api
+	 */
+	public function render() {
+		return $this->view->render();
+	}
+
+	/**
+	 * Initializes this view.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeView() {
+		$this->view->initializeView();
+	}
+
+	/**
+	 * @param string $layoutRootPath
+	 */
+	public function setLayoutRootPath($layoutRootPath) {
+		$this->view->setLayoutRootPath($layoutRootPath);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLayoutRootPath() {
+		return $this->view->getLayoutRootPath();
+	}
+
+	/**
+	 * @param string $partialRootPath
+	 */
+	public function setPartialRootPath($partialRootPath) {
+		$this->view->setPartialRootPath($partialRootPath);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPartialRootPath() {
+		return $this->view->getPartialRootPath();
+	}
+
+	/**
+	 * @param string $templatePathAndFilename
+	 */
+	public function setTemplatePathAndFilename($templatePathAndFilename) {
+		$this->view->setTemplatePathAndFilename($templatePathAndFilename);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTemplatePathAndFilename() {
+		return $this->view->getTemplatePathAndFilename();
+	}
+
+	/**
+	 * @param string $templateSource
+	 */
+	public function setTemplateSource($templateSource) {
+		$this->view->setTemplateSource($templateSource);
+	}
 
 }
